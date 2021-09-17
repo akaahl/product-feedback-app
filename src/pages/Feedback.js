1037db5c73c43391c20338f4a27af59206f7ceb4
a.k.a.a.h.l <a.k.afiq@live.com>
2021-09-17 19:11:50 +0800

    Styled the nav buttons in Feedback component

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,14 +5,14 @@ import arrowLeftIcon from "../assets/shared/icon-arrow-left.svg";
 const Feedback = () => {
   return (
     <StyledFeedback>
-      <div className="feedback__header">
-        <button>
+      <nav className="feedback__nav">
+        <button className="feedback__nav-back-btn">
           <img src={arrowLeftIcon} alt="arrow left" />
           Go Back
         </button>
 
-        <button>Edit Feedback</button>
-      </div>
+        <button className="feedback__nav-edit-btn">Edit Feedback</button>
+      </nav>
     </StyledFeedback>
   );
 };
@@ -22,4 +22,50 @@ export default Feedback;
 const StyledFeedback = styled.main`
   margin: 70px 0;
   width: 800px;
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .feedback__nav-back-btn {
+      border: none;
+      background: none;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      color: #647196;
+      font-weight: 600;
+      font-size: 16px;
+
+      img {
+        margin-right: 15px;
+        height: 10px;
+        width: 8px;
+      }
+    }
+
+    .feedback__nav-edit-btn {
+      border: none;
+      background: none;
+      border-radius: 10px;
+      padding: 10px 25px;
+      color: #ffffff;
+      background-color: #4661e6;
+      font-size: 14px;
+      font-weight: 600;
+      cursor: pointer;
+      transition: background-color 0.3s ease-in-out;
+      box-shadow: rgba(36, 26, 26, 0.2) 0px 2px 1px -1px,
+        rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;
+
+      &:hover {
+        background-color: #7c91f9;
+      }
+
+      &:active {
+        transform: scale(0.95);
+      }
+    }
+  }
 `;
